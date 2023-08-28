e24968546f10064b415095b3376e6eb8d6f19210
Chris <karampagiaschris@gmail.com>
2023-08-28 20:00:45 +0300

    Create animations for header & main

---
+++ script.js
@@ -0,0 +1,25 @@
+function reveal() {
+  let projects = document.querySelectorAll(".project");
+  let footer = document.querySelector("footer");
+  let windowHeight = window.innerHeight;
+  let projectVisible = windowHeight * 0.5;
+  for (let i = 0; i < projects.length; i += 2) {
+    let projectDist = projects[i].getBoundingClientRect().top;
+    if (projectDist < windowHeight - projectVisible) {
+      projects[i].classList.add("reveal-right-rotate-counter-clockwise");
+    } else {
+      projects[i].classList.remove("reveal-right-rotate-counter-clockwise");
+    }
+  }
+
+  for (let i = 1; i < projects.length; i += 2) {
+    let projectDist = projects[i].getBoundingClientRect().top;
+    if (projectDist < windowHeight - projectVisible) {
+      projects[i].classList.add("reveal-right-rotate-clockwise");
+    } else {
+      projects[i].classList.remove("reveal-right-rotate-clockwise");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
